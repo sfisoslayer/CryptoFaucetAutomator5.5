2fb1935c3de47ec2ac4a7752a7ed3ed53424725a
sfisoslayer <crimsionbruno@gmail.com>
2025-07-02 01:23:44 +0200

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -609,7 +609,7 @@ app.include_router(api_router)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=["*"],
+    allow_origins=["https://crypto-faucet-frontend.onrender.com", "*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
